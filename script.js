b4ef65d36dfcb63938e74f20c8a8ccfb94a62851
cosmicdreams02 <95249832+cosmicdreams02@users.noreply.github.com>
2022-05-18 16:54:28 +0200

    Upload

--- script.js
+++ script.js
@@ -7,8 +7,27 @@ btn.addEventListener('click', (e) => {
   fetch(apiUrl)
     .then((response) => response.json())
     .then(({message}) => {
-     
-      console.log(message);
+      let imagen = document.querySelector('.perroprincipal');
+      imagen.setAttribute('src', message);
+      let array=message.split('/')
       
+      localStorage.setItem(array[4], message);
+      let box = document.querySelector('#dos');
+      let url = localStorage.getItem(array[4])
+      box.setAttribute('src',url );
+      console.log(box);
     });
 });
+
+function recorridoRazas () {
+
+
+
+  for(let i=0; i<localStorage.length; i++) {
+    let key = localStorage.key(i);
+    alert(`${key}: ${localStorage.getItem(key)}`);
+  }
+  
+}
+
+

bf30bcde345675c0ef77451145753e5394e0877a
cosmicdreams02 <95249832+cosmicdreams02@users.noreply.github.com>
2022-05-22 17:05:06 +0200

    Final

--- script.js
+++ script.js
@@ -10,40 +10,91 @@ Cuando se haya completado todo el álbum aparecerá una gráfica de todas las ra
 
 Las imágenes ilustran el funcionamiento, pero la apariencia de la aplicación depende del grupo y se debe cuidar tanto como el funcionamiento; además se deben usar conocimientos de Flexbox y Grid en la medida de las posibilidades del diseño. */
 
-
 const apiUrl = 'https://dog.ceo/api/breeds/image/random';
 
 const btn = document.querySelector('#perro');
 
-btn.addEventListener('click', (e) => {
+btn.addEventListener('click', () => {
   let arrayURL = [];
   let obj = {};
   let i = 0;
-  if (localStorage.getItem('perritos') == null) {  // Se busca la clave perrito, si no esta nos devuelve null.
-    localStorage.setItem('perritos', JSON.stringify([])); // En caso de no estar la creamos con un objeto vacío que contenga una cadena.
+  if (localStorage.getItem('perritos') == null) {         // Se busca la clave perrito, si no esta nos devuelve null.
+    localStorage.setItem('perritos', JSON.stringify([]));  // En caso de no estar la creamos con un objeto vacío que contenga una cadena.
   }
 
-  fetch(apiUrl)    // Llamada a la url.
-    .then((response) => response.json())   // La promesa nos devuevle un JSON, y nos lo convierte a un objeto. Sigue siendo una promesa.
-    .then(({ message }) => {               // De la promesa resuelta, se extrae el "message".
-      let imagen = document.querySelector('.perroprincipal');  // Creamos una variable imagen donde almacenamos la url de la imagen principal.
-  
-      imagen.setAttribute('src', message);   // A esa imagen al atributo src le damos el valor del message que extrajimos es decir la url.
+  // Llamada a la url.
+   // La promesa nos devuevle un JSON, y nos lo convierte a un objeto. Sigue siendo una promesa.
+   // Creamos una variable imagen donde almacenamos la url de la imagen principal.
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then(({ message }) => {
+      let imagen = document.querySelector('.perroprincipal');
+
+      imagen.setAttribute('src', message);  // A esa imagen al atributo src le damos el valor del message que extrajimos es decir la url.
       let array = message.split('/');  // Cogemos la url y la dividimos cada vez que haya / gracias a .split. Y la almacenamos en la variable array como array.
 
-      arrayURL = JSON.parse(localStorage.getItem('perritos'));   // Cargo del localstorage lo que tengo dentro de la clave "perritos", y lo convierto con .parse a un array. Y lo almacenamos en array URL.
+      let descripcion = document.querySelector('#descripcion');
+      descripcion.textContent = array[4];
+
+      arrayURL = JSON.parse(localStorage.getItem('perritos'));  // Cargo del localstorage lo que tengo dentro de la clave "perritos", y lo convierto con .parse a un array. Y lo almacenamos en array URL.
       let busqueda = arrayURL.find((item) => item.id == array[4]);  // Comprobar si la raza ya existe. Es decir que la raza sea la misma clave que hemos guardado array.
 
-      if (busqueda == undefined) {  // Con esta condición comprobamos si la raza no se ha repetido. 
-        obj = { [array[4]]: message };  // Creo un objeto que tenga la clave raza: con valor url es decir "message".
-        arrayURL.push(obj);   // Ahora al array le agregamos el objeto.
 
-        arrayURL.forEach((element) => {   // Recorremos el array.
-          let valores = Object.values(element);  // // En la variable valores guardamos un nuevo array solo con los valores del objeto. (LAS URL).
-          valores.forEach((elem) => { // Recorremos las url.
-            i++;                      // Usamos el contador i para coger los selectores de las imagenes. 
-            let img = document.querySelector(`#img${i}`);
-            img.setAttribute('src', elem);
+      if (busqueda == undefined) { // Con esta condición comprobamos si la raza no se ha repetido. 
+        obj = { [array[4]]: message }; // Creo un objeto que tenga la clave raza: con valor url es decir "message".
+        arrayURL.push(obj);  // Ahora al array le agregamos el objeto.
+
+        arrayURL.forEach((element) => {  // Recorremos el array.
+          let valores = Object.values(element);  // En la variable valores guardamos un nuevo array solo con los valores del objeto. (LAS URL).
+          valores.forEach((elem) => {  // Recorremos las url.
+            i++;                       // Usamos el contador i para coger los selectores de las imagenes. 
+            if (i <= 20) {             // El contador de imagenes sea menor o igual que 20.
+              let img = document.querySelector(`#img${i}`); // Almacenamos en la variable imagen, la primera imagen.
+              img.setAttribute('src', elem); // Cogemos el elmento y le introdocimos la url de la imagen.
+            }
+
+            if (i == 20) {   // Creamos una condicion para que tenga de tope 20 imagenes igual que nuestra cuadricula.
+              btn.disabled = true;  // Cuando se cumpla la condición que se desactive el botón.
+              let razas = []; // Creamos una array para razas para la gráfica.
+              let serie = []; // Creamos una array para serie para la gráfica.
+
+              arrayURL.forEach((element) => { // Recorremos el array.
+                let claves = Object.keys(element); // En la variable claves guardamos un nuevo array con las claves.
+                claves.forEach((elem) => {     //Recorremos las claves.
+                  razas.push(elem);           // Ahora al array le agregamos las claves.
+                });
+              });
+
+              for (let i = 0; i < 20; i++) { // Creamos un bucle para que me de 20 valores aleatorios. 
+                serie.push(Math.floor(Math.random() * 10)); // Ahora al array le agregamos 20 numeros aleatorios en tre 0 y 10. Usamos math.floor para redondeaer lo que nos devuelve math.random.
+              }
+
+              // Gráfica
+              let data = {
+                labels: [...razas], // Desestructuramos el objeto con ...
+
+                series: [[...serie]], // Desestructuramos el objeto con ...
+              };
+              let options = { 
+                width: window.screen.width - (window.screen.width * 0, 20),
+                height: 400,
+              };
+
+              new Chartist.Bar('.ct-chart', data, options).on(
+                'draw',
+                function (bars) {
+                  bars.element.animate({
+                    y2: {
+                      begin: 0,
+                      dur: 500,
+                      from: bars.y1,
+                      to: bars.y2,
+                    },
+                  });
+                }
+              );
+            }
           });
         });
         localStorage.setItem('perritos', JSON.stringify(arrayURL));
@@ -52,7 +103,3 @@ btn.addEventListener('click', (e) => {
       }
     });
 });
-
-
-
-
